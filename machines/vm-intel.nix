f46e71096646f4175b6e6c0ceac8d25c8bcdfdb0
Aditya Thebe <contact@adityathebe.com>
2024-03-03 14:51:33 +0545

    feat: home manager zsh and atuin

--- machines/vm-intel.nix
+++ machines/vm-intel.nix
@@ -14,8 +14,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "charali"; # Define your hostname.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.hostName = "charali";
+  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Asia/Kathmandu";
@@ -81,10 +81,8 @@
     DOCKER_CONFIG = "$XDG_CONFIG_HOME/docker";
     GNUPGHOME = "$XDG_DATA_HOME/gnupg";
     GOPATH = "$XDG_DATA_HOME/go";
-    HISTFILE = "$XDG_STATE_HOME/zsh/history";
     LESSHISTFILE = "-";
     NODE_REPL_HISTORY = "$XDG_DATA_HOME/node_repl_history";
-    ZDOTDIR = "$HOME/.config/zsh";
 
     GIT_EDITOR = "nvim";
     EDITOR = "nvim";
@@ -108,70 +106,7 @@
     };
 
     zsh = {
-      promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      ohMyZsh = {
-        enable = true;
-        plugins = [
-          "systemd"
-        ];
-      };
-      enableCompletion = true;
       enable = true;
-      shellAliases = {
-        k9="killall -9";
-        k15="killall -15";
-
-        mv="mv -iv";
-        rm="rm -v";
-
-        ls="eza -h --color=auto --group-directories-first";
-        ll="eza -l";
-        lh="eza -alF";
-        lg="eza -alF | grep -i";
-        
-        sudo="sudo ";
-
-        digs="dig +short";
-        curldoh="curl --doh-url https://cloudflare-dns.com/dns-query";
-        publicip="dig +short myip.opendns.com @resolver1.opendns.com -4";
-        
-        downl="aria2c -c -x 8 -s 8";
-        qrgen=" qrencode -t ANSI";
-
-        # alias watch to itself so it works with other aliases. Pretty cool actually.
-        watch="watch ";
-
-        # Program alternatives
-        cat="bat";
-
-        # Git
-        cdr="cd $(git rev-parse --show-toplevel)"; # jumps to the root path of a git repository
-        gb="git branch";
-        gca="git commit --amend'";
-        gco="git checkout'";
-        gp="git push'";
-        grh="git reset --hard'";
-        gu="git reset HEAD"; # unstage a file
-
-        vim="nvim";
-        du="du -ch";
-        df="df -h";
-        wget="wget --hsts-file=\"$XDG_DATA_HOME/wget-hsts\""; # In order to avoid history file in $HOME"
-
-        # Audio and video downloaders from spotify and youtube
-        yt="yt-dlp --add-metadata -f 'bestvideo[ext=mp4]'";
-        yta="yt-dlp --downloader aria2c";
-
-        # speedtest: get a 100MB file via wget
-        speedtest="wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test100.zip";
-
-        # Weather Report
-        weather="curl https://wttr.in/kathmandu";
-
-        # URL encode and decode
-        urlencode="python -c \"import sys, urllib.parse as ul; [sys.stdout.write(ul.quote_plus(l)) for l in sys.stdin]\"'";
-        urldecode="python -c \"import sys, urllib.parse as ul; [sys.stdout.write(ul.unquote_plus(l)) for l in sys.stdin]\"'";
-      };
     };
   };
 
--- users/aditya/home.nix
+++ users/aditya/home.nix
@@ -13,9 +13,110 @@
     ];
   };
 
+  programs.atuin = {
+    enable = true;
+    enableZshIntegration = true;
+    flags = [ "--disable-up-arrow" ];
+    settings = {
+      search_mode = "fuzzy";
+      filter_mode = "host";
+      filter_mode_shell_up_key_binding = "host";
+      style = "full";
+      show_preview = true;
+      scroll_context_lines = 3;
+      max_preview_height = 4;
+      enter_accept = true;
+      keymap_mode = "vim-insert";
+    };
+  };
+
   programs.nix-index = {
     enable = true;
     enableZshIntegration = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+
+    dotDir = ".config/zsh";
+    initExtra = "source ~/.config/zsh/.p10k.zsh";
+
+    syntaxHighlighting.enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+
+    history = {
+      size = 5000000000;
+      save = 5000000000;
+      path = ".local/share/zsh/history";
+    };
+
+    plugins = [
+      { name = "powerlevel10k"; src = pkgs.zsh-powerlevel10k; file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";}
+    ];
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "systemd"
+      ];
+    };
+
+    shellAliases = {
+      k9="killall -9";
+      k15="killall -15";
+
+      mv="mv -iv";
+      rm="rm -v";
+
+      ls="eza -h --color=auto --group-directories-first";
+      ll="eza -l";
+      lh="eza -alF";
+      lg="eza -alF | grep -i";
+      
+      sudo="sudo ";
+
+      digs="dig +short";
+      curldoh="curl --doh-url https://cloudflare-dns.com/dns-query";
+      publicip="dig +short myip.opendns.com @resolver1.opendns.com -4";
+      
+      downl="aria2c -c -x 8 -s 8";
+      qrgen=" qrencode -t ANSI";
+
+      # alias watch to itself so it works with other aliases. Pretty cool actually.
+      watch="watch ";
+
+      # Program alternatives
+      cat="bat";
+
+      # Git
+      cdr="cd $(git rev-parse --show-toplevel)"; # jumps to the root path of a git repository
+      gb="git branch";
+      gca="git commit --amend'";
+      gco="git checkout'";
+      gp="git push'";
+      grh="git reset --hard'";
+      gu="git reset HEAD"; # unstage a file
+
+      vim="nvim";
+      du="du -ch";
+      df="df -h";
+      wget="wget --hsts-file=\"$XDG_DATA_HOME/wget-hsts\""; # In order to avoid history file in $HOME"
+
+      # Audio and video downloaders from spotify and youtube
+      yt="yt-dlp --add-metadata -f 'bestvideo[ext=mp4]'";
+      yta="yt-dlp --downloader aria2c";
+
+      # speedtest: get a 100MB file via wget
+      speedtest="wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test100.zip";
+
+      # Weather Report
+      weather="curl https://wttr.in/kathmandu";
+
+      # URL encode and decode
+      urlencode="python -c \"import sys, urllib.parse as ul; [sys.stdout.write(ul.quote_plus(l)) for l in sys.stdin]\"'";
+      urldecode="python -c \"import sys, urllib.parse as ul; [sys.stdout.write(ul.unquote_plus(l)) for l in sys.stdin]\"'";
+    };
   };
 
   programs.git = {
